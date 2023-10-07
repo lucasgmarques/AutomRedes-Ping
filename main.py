d1e27b83af1630df87223bb5249b2d03dc299b93
Lucas Marques <lucasg3.gdh@gmail.com>
2023-10-06 21:29:48 -0300

    Trailing spaces removed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import datetime
-from prettytable import PrettyTable
 import re
+from prettytable import PrettyTable
 
 # Formato da data e hora
 FORMATO_DATA = "%d-%m-%Y"
@@ -12,7 +12,7 @@ def execute_ping(url, count=2):
     os.system(ping_command)
 
 def read_ping_result(log_file):
-    with open(log_file, "r") as file:
+    with open(log_file, "r", encoding='utf8') as file:
         result = file.read()
     return result
 
@@ -27,14 +27,36 @@ def extract_ip(result):
                 break
     return ip_address
 
+def extract_avg_time(result):
+    for line in result.splitlines():
+        if "rtt min/avg/max/mdev" in line:
+            stats_data = re.search(r'\/(\d+\.\d{3})\/', line)
+            if stats_data:
+                return stats_data.group(1)
+    return "N/A"
+
 def create_table(url, ip_address, time_avg):
-    # Obtém a data e hora atual
     current_time = datetime.datetime.now()
 
-    # Cria a tabela c/ PrettyTable
     table = PrettyTable()
-    table.field_names = ["Data", "Horário", "URL", "IP", "Status", "TTL", "Time (média)", "Pacotes perdidos"]
-    table.add_row([current_time.strftime(FORMATO_DATA), current_time.strftime(FORMATO_HORA), url, ip_address, "Online", "N/A", time_avg, "N/A"])
+    table.field_names = [
+                         "Data", 
+                         "Horário", 
+                         "URL", 
+                         "IP", 
+                         "Status", 
+                         "TTL", 
+                         "Time (média)", 
+                         "Pacotes Perdidos",
+                         ]
+    table.add_row([current_time.strftime(FORMATO_DATA),
+                   current_time.strftime(FORMATO_HORA),
+                   url, 
+                   ip_address,
+                   "Online", 
+                   "N/A", 
+                   time_avg,
+                   "N/A"])
 
     return table
 
@@ -52,24 +74,17 @@ def ping_url(url):
 
         # Extrai o IP
         ip_address = extract_ip(result)
+        avg = extract_avg_time(result)
 
         if ip_address:
-            for line in result.splitlines():
-                if "rtt min/avg/max/mdev" in line:
-                    stats_data = re.search(r'\/(\d+\.\d{3})\/', line)
-                    if stats_data:
-                        time_avg = stats_data.group(1)
-                    else:
-                        time_avg = "N/A"
-                    table = create_table(url, ip_address, time_avg)
-                    print(table)
+            table = create_table(url, ip_address, avg)
+            print(table)
         else:
             print("Não foi possível encontrar o IP na saída do ping.")
 
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
     finally:
-        # Remove o arquivo de log após o uso
         if os.path.exists(log_file):
             os.remove(log_file)
 
@@ -82,15 +97,14 @@ def main():
             break
         print("-------------------------- PINGANDO -----------------------")
         ping_url(url)
-        
-        option = input("Deseja continuar? [S]im [N]ao: ")
+        while True:
+            option = input("Deseja continuar? [S]im [N]ao: ")
 
-        if option.upper() == 'S':
-            continue
-        if option.upper() == 'N':
-            print('#################### FIM DE PROGRAMA ####################')
-            break
-        else:
+            if option.upper() == 'S':
+                break
+            if option.upper() == 'N':
+                print('#################### FIM DE PROGRAMA ####################')
+                return
             print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
